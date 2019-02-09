16a93ba8eb17d23dff474f77aa3810db4674fb33
KeertiPriyaKS30 <47452120+KeertiPriyaKS30@users.noreply.github.com>
2019-02-09 16:55:25 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+   int a;
+   scanf("%d",&a);
+   if(a%2==0)
+   {
+     printf("Even");
+   }
+   else if(a%2>0)
+   {
+     printf("Odd");
+   }
+   else if(a%2<=0)
+   {
+       printf("invalid");
+   }
+   return 0;
+   }
